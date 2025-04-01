eba0a7b928317d9c6fe258af71d1a66ca91c3966
Cabr1to <luis.b.c.melo@gmail.com>
2025-04-01 01:34:49 -0300

    Projeto API, exercicio 4

---
+++ Projeto_API/frontend/vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, 'api')
+      }
+    }
+  }
+}})
